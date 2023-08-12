67f82e68a4b68ff30e7c6683ab93d54cb6bf7a05
RootMusicWorks <142084337+RootMusicWorks@users.noreply.github.com>
2023-08-12 19:05:52 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ startStopButton.addEventListener('click', toggleMetronome);
 
 const startPositions = ['6弦 人差し指', '6弦 中(薬)指', '6弦 子指', '5弦 人差し指', '5弦 中(薬)指', '5弦 子指'];
 const keys = ['C','C#','Db','D', 'D#','Eb', 'E', 'F','F#','Gb', 'G','G#','Ab', 'A','A#','Bb', 'B'];
-const scales = ['Major', 'Minor', 'Minor Pentatonic','Major Pentatonic', 'Dorian','Mixsolydian','Melodic Minor','Altered','Super Lydian',];
+const scales = ['Major', 'Minor', 'Major Pentatonic','Minor Pentatonic', 'Dorian','Mixsolydian','Melodic Minor','Altered','Super Lydian',];
 
 const generateButton = document.querySelector('#generateButton');
 const infoDisplay = document.querySelector('#infoDisplay');
@@ -37,9 +37,43 @@ const infoDisplay = document.querySelector('#infoDisplay');
 generateButton.addEventListener('click', generateRandomInfo);
 
 function generateRandomInfo() {
+    // 選択されたスケールを格納する配列を準備
+    const selectedScales = [];
+
+    // 各チェックボックスの状態を確認し、選択されていれば配列に追加
+    if (document.querySelector('#majorCheckbox').checked) {
+        selectedScales.push('Major');
+    }
+    if (document.querySelector('#minorCheckbox').checked) {
+        selectedScales.push('Minor');
+    }
+    if (document.querySelector('#majorpentaCheckbox').checked) {
+        selectedScales.push('Major Pentatonic');
+    }
+    if (document.querySelector('#minorpentaCheckbox').checked) {
+        selectedScales.push('Minor Pentatonic');
+    }
+    if (document.querySelector('#drianCheckbox').checked) {
+        selectedScales.push('Dorian');
+    }
+    if (document.querySelector('#mixsolydianCheckbox').checked) {
+        selectedScales.push('Mixsolydian');
+    }
+    if (document.querySelector('#melodicminorCheckbox').checked) {
+        selectedScales.push('Melodic Minor');
+    }
+    if (document.querySelector('#AlteredCheckbox').checked) {
+        selectedScales.push('Altered');
+    }
+    if (document.querySelector('#superlydianCheckbox').checked) {
+        selectedScales.push('Super Lydian');
+    }
+    // 他のスケールのチェックボックスも同様に追加
+
+    // 選択されたスケールの中からランダムに選択
     const randomStartPosition = startPositions[Math.floor(Math.random() * startPositions.length)];
     const randomKey = keys[Math.floor(Math.random() * keys.length)];
-    const randomScale = scales[Math.floor(Math.random() * scales.length)];
+    const randomScale = selectedScales[Math.floor(Math.random() * selectedScales.length)];
 
     const infoText = `Start Position: ${randomStartPosition}<br>Key: ${randomKey}<br>Scale: ${randomScale}`;
     infoDisplay.innerHTML = infoText;
