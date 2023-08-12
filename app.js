9eb3cf99375308691b7b9991728fd2b0e881fe27
RootMusicWorks <142084337+RootMusicWorks@users.noreply.github.com>
2023-08-12 18:02:39 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+const tempoInput = document.querySelector('#tempo');
+const startStopButton = document.querySelector('#startStopButton');
+let isMetronomeRunning = false;
+let metronomeInterval;
+
+function playClick() {
+    const clickSound = new Audio('click.mp3'); // 音声ファイルのパスを指定
+    clickSound.play();
+}
+
+
+function toggleMetronome() {
+    if (isMetronomeRunning) {
+        clearInterval(metronomeInterval);
+        startStopButton.textContent = 'Start';
+    } else {
+        const tempo = parseInt(tempoInput.value, 10);
+        const intervalTime = 60000 / tempo; // ミリ秒単位のインターバル
+
+        metronomeInterval = setInterval(playClick, intervalTime);
+        startStopButton.textContent = 'Stop';
+    }
+    isMetronomeRunning = !isMetronomeRunning;
+}
+
+startStopButton.addEventListener('click', toggleMetronome);
+
+
+
 const startPositions = ['6弦 人差し指', '6弦 中(薬)指', '6弦 子指', '5弦 人差し指', '5弦 中(薬)指', '5弦 子指'];
 const keys = ['C','C#','Db','D', 'D#','Eb', 'E', 'F','F#','Gb', 'G','G#','Ab', 'A','A#','Bb', 'B'];
 const scales = ['Major', 'Minor', 'Minor Pentatonic','Major Pentatonic', 'Dorian','Mixsolydian','Melodic Minor','Altered','Super Lydian',];
